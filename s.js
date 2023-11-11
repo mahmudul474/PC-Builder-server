10885ba5b1cca2eb16b53d5412b4ee2f5d308a57
mahmudul474 <mdmahmudulla474@gmail.com>
2023-11-11 09:40:37 +0600

    CHANGE server

--- s.js
+++ s.js
@@ -1,19 +1,20 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const port = process.env.PORT || 5000;
-const { MongoClient } = require("mongodb");
+const port = 5000;
 
 app.use(cors());
 app.use(express.json());
-uri ="mongodb+srv://ass:z3K14p1XiRjqkt1j@softopark.ockrkce.mongodb.net/?retryWrites=true&w=majority";
- // Use environment variable for MongoDB connection string
+const { MongoClient } = require("mongodb");
+
+// Replace the uri string with your connection string.
+uri =
+  "mongodb+srv://ass:z3K14p1XiRjqkt1j@softopark.ockrkce.mongodb.net/?retryWrites=true&w=majority";
+
 const client = new MongoClient(uri);
 
 async function run() {
   try {
-    await client.connect();
-
     const featuresProductCollections = client
       .db("Assainment-6")
       .collection("featuresProduct");
@@ -25,21 +26,20 @@ async function run() {
       const categories = await featuresCategoryCollections.find().toArray();
       res.send(categories);
     });
-
-    // Add other routes...
+    app.get("/category/:id", async (req, res) => {});
+    app.get("/product", async (req, res) => {});
+    app.get("/product/:id", async (req, res) => {});
 
     app.post("/product", async (req, res) => {
       const product = req.body;
-      const result = await featuresProductCollections.insertOne(product);
+      const result = await featuresCategoryCollections.insertOne(product);
       res.send(result);
     });
   } finally {
- 
   }
 }
+run().catch(console.dir);
 
-run().then(() => {
-  app.listen(port, () => {
-    console.log("server running on port", port);
-  });
-}).catch(console.error);
+app.listen(port, (req, res) => {
+  console.log("sever running on port", port);
+});
